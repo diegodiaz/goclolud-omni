122e076832d57ce4512aa81b259352023c052fbd
Diego Diaz <ddiaz@scpsoluciones.com>
2019-11-24 22:48:42 -0300

    Se incluye express-validation y se valida el request

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 // Route api user login 
-router.post('/user/login', (req, res)=>{
-  res.json({success:true, token:''});
-});
+router.post('/user/login', [
+    check('username').not().isEmpty().withMessage('Name is required'),
+    check('password', 'Password is required').not().isEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array());
+    } else {
+      res.send({});
+    }
+  }
+);
 // Route api user validate is login 
 router.post('/user/validate', (req, res)=>{
   res.json({success:true});
